922dd6255f7f44faa8bf70aa418adf97ed591c8c
Gabeperson <95374687+Gabeperson@users.noreply.github.com>
2025-01-16 08:25:29 -0800

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,233 +1,260 @@
 #![forbid(unsafe_code)]
-use std::{
-    future::Future,
-    pin::Pin,
-    sync::{Arc, LazyLock, Mutex},
-    task::{Context, Poll, Wake, Waker},
-};
+use std::rc::Rc;
+use std::{cell::Cell, future::Future};
 
-pub struct Yielder<Y> {
-    sender: Sender<Y>,
-}
+mod yielder {
+    use std::{cell::Cell, rc::Rc, task::Poll};
 
-struct Sender<T>(Arc<Mutex<Option<T>>>);
-
-struct Receiver<T>(Arc<Mutex<Option<T>>>);
-
-impl<T> Sender<T> {
-    fn send(&mut self, val: T) {
-        let mut lock = self.0.try_lock().expect("Caller should be waiting");
-        *lock = Some(val);
+    pub struct Yielder<Y> {
+        pub(crate) sender: Sender<Y>,
     }
-}
 
-impl<T> Receiver<T> {
-    /// Take the value out of the inner mutex and panic if there is none
-    fn try_recv(&mut self) -> Option<T> {
-        let mut lock = self.0.try_lock().expect(
-            "Generator should have yielded. (Can happen if called in a task or another thread)",
-        );
-        lock.take()
-    }
-}
-
-impl<Y> Yielder<Y> {
-    pub fn yield_value(&mut self, value: Y) -> impl Future<Output = ()> {
-        YieldFuture {
-            sender: &mut self.sender,
-            value: Some(value),
-        }
-    }
-}
-
-struct YieldFuture<'a, Y> {
-    sender: &'a mut Sender<Y>,
-    value: Option<Y>,
-}
-
-impl<Y> Unpin for YieldFuture<'_, Y> {}
-
-impl<Y> Future for YieldFuture<'_, Y> {
-    type Output = ();
-
-    fn poll(
-        mut self: std::pin::Pin<&mut Self>,
-        _cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Self::Output> {
-        match self.value.take() {
-            // First time being polled, yield so the caller (of the generator) regains control
-            Some(v) => {
-                self.sender.send(v);
-                Poll::Pending
+    impl<Y> Yielder<Y> {
+        pub fn yield_value(&mut self, value: Y) -> impl Future<Output = ()> {
+            YieldFuture {
+                sender: &mut self.sender,
+                value: Some(value),
             }
-            // Second time being polled
-            None => Poll::Ready(()),
         }
     }
-}
 
-struct NoopWake;
-impl Wake for NoopWake {
-    fn wake(self: Arc<Self>) {}
-}
+    // #[cfg(not(feature = "send"))]
+    #[repr(transparent)]
+    pub(crate) struct Sender<T>(pub(crate) Rc<Cell<Option<T>>>);
 
-pub struct MiniGen<'a, Y, R> {
-    future: Pin<Box<dyn Future<Output = R> + 'a>>,
-    receiver: Receiver<Y>,
-    finished: bool,
-}
-
-#[repr(transparent)]
-pub struct MiniIter<'a, Y>(MiniGen<'a, Y, ()>);
-
-impl<Y> Iterator for MiniIter<'_, Y> {
-    type Item = Y;
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.0.resume() {
-            GeneratorStatus::Yielded(it) => Some(it),
-            GeneratorStatus::Returned(()) | GeneratorStatus::Completed => None,
+    impl<T> Sender<T> {
+        pub(crate) fn send(&mut self, val: T) {
+            let old = self.0.replace(Some(val));
+            assert!(old.is_none(), "sending, but channel isn't empty!");
         }
     }
-}
 
-impl<Y> MiniIter<'_, Y> {
-    pub fn resume(&mut self) -> impl Future<Output = Option<Y>> {
-        MiniIterFuture {
-            generator: &mut *self,
+    #[repr(transparent)]
+    pub(crate) struct Receiver<T>(pub(crate) Rc<Cell<Option<T>>>);
+
+    impl<T> Receiver<T> {
+        pub(crate) fn try_recv(&mut self) -> Option<T> {
+            self.0.take()
         }
     }
-}
 
-struct MiniIterFuture<'r, 'g, Y> {
-    generator: &'r mut MiniIter<'g, Y>,
-}
+    struct YieldFuture<'a, Y> {
+        sender: &'a mut Sender<Y>,
+        value: Option<Y>,
+    }
 
-impl<Y> Future for MiniIterFuture<'_, '_, Y> {
-    type Output = Option<Y>;
+    impl<Y> Unpin for YieldFuture<'_, Y> {}
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        // The future was polled to completion before, so we should act similar to a
-        // fused iterator and return that it's completed
-        if self.generator.0.finished {
-            return Poll::Ready(None);
-        }
-        let pinned = self.generator.0.future.as_mut();
-        // We poll the future we're storing
-        match pinned.poll(cx) {
-            // Future has returned, generator has completed.
-            Poll::Ready(()) => {
-                self.generator.0.finished = true;
-                Poll::Ready(None)
-            }
-            // Yield or a random await
-            Poll::Pending => {
-                // We check here, because the future might yield for some reason other than
-                // stream.yield_value()
-                if let Some(v) = self.generator.0.receiver.try_recv() {
-                    return Poll::Ready(Some(v));
+    impl<Y> Future for YieldFuture<'_, Y> {
+        type Output = ();
+
+        fn poll(
+            mut self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Self::Output> {
+            match self.value.take() {
+                // First time being polled, yield so the caller (of the generator) regains control
+                Some(v) => {
+                    self.sender.send(v);
+                    Poll::Pending
                 }
-                Poll::Pending
+                // Second time being polled
+                None => Poll::Ready(()),
             }
         }
     }
 }
 
-#[cfg(feature = "stream")]
-impl<Y> futures_core::Stream for MiniIter<'_, Y> {
-    type Item = Y;
+mod generator {
+    use std::{
+        pin::Pin,
+        sync::Arc,
+        task::{Context, Poll, Wake},
+    };
 
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let fut = self.resume();
-        let fut = std::pin::pin!(fut);
-        fut.poll(cx)
+    use crate::yielder::*;
+
+    struct NoopWake;
+    impl Wake for NoopWake {
+        fn wake(self: Arc<Self>) {}
     }
-}
 
-#[derive(Clone, Debug, Copy, PartialEq, Eq)]
-pub enum GeneratorStatus<Y, R> {
-    Yielded(Y),
-    Returned(R),
-    Completed,
-}
+    pub(crate) use backend::MiniGen;
+    mod backend {
+        use std::{
+            pin::Pin,
+            sync::{Arc, LazyLock},
+            task::{Context, Poll, Waker},
+        };
 
-impl<Y, R> MiniGen<'_, Y, R> {
-    pub fn resume(&mut self) -> GeneratorStatus<Y, R> {
-        // The future was polled to completion before, so we should act similar to a
-        // fused iterator and return that it's completed
-        if self.finished {
-            return GeneratorStatus::Completed;
+        use crate::generator::NoopWake;
+
+        use super::{GeneratorStatus, Receiver};
+
+        pub struct MiniGen<'a, Y, R> {
+            pub(crate) future: Pin<Box<dyn Future<Output = R> + 'a>>,
+            pub(crate) receiver: Receiver<Y>,
+            pub(crate) finished: bool,
         }
-        // We don't actually need a proper "waker" so we can just sort of no-op it
-        static WAKER: LazyLock<Waker> = LazyLock::new(|| Waker::from(Arc::new(NoopWake)));
-        let mut context = Context::from_waker(&WAKER);
-        loop {
-            let pinned = self.future.as_mut();
-            // We poll the future we're storing
-            match pinned.poll(&mut context) {
-                // Future has returned, generator has completed.
-                Poll::Ready(v) => {
-                    self.finished = true;
-                    return GeneratorStatus::Returned(v);
+
+        impl<Y, R> MiniGen<'_, Y, R> {
+            pub fn resume(&mut self) -> GeneratorStatus<Y, R> {
+                // The future was polled to completion before, so we should act similar to a
+                // fused iterator and return that it's completed
+                if self.finished {
+                    return GeneratorStatus::Completed;
                 }
-                // Yield or a random await
-                Poll::Pending => {
-                    // We check here, because the future might yield for some reason other than
-                    // stream.yield_value()
-                    if let Some(v) = self.receiver.try_recv() {
-                        return GeneratorStatus::Yielded(v);
+                // We don't actually need a proper "waker" so we can just sort of no-op it
+                static WAKER: LazyLock<Waker> = LazyLock::new(|| Waker::from(Arc::new(NoopWake)));
+                let mut context = Context::from_waker(&WAKER);
+                loop {
+                    let pinned = self.future.as_mut();
+                    // We poll the future we're storing
+                    match pinned.poll(&mut context) {
+                        // Future has returned, generator has completed.
+                        Poll::Ready(v) => {
+                            self.finished = true;
+                            return GeneratorStatus::Returned(v);
+                        }
+                        // Yield or a random await
+                        Poll::Pending => {
+                            // We check here, because the future might yield for some reason other than
+                            // stream.yield_value()
+                            if let Some(v) = self.receiver.try_recv() {
+                                return GeneratorStatus::Yielded(v);
+                            }
+                        }
+                    }
+                }
+            }
+
+            pub fn resume_async(&mut self) -> impl Future<Output = GeneratorStatus<Y, R>> {
+                MiniGenFuture {
+                    generator: &mut *self,
+                }
+            }
+        }
+
+        struct MiniGenFuture<'r, 'g, Y, R> {
+            generator: &'r mut MiniGen<'g, Y, R>,
+        }
+
+        impl<Y, R> Future for MiniGenFuture<'_, '_, Y, R> {
+            type Output = GeneratorStatus<Y, R>;
+
+            fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+                // The future was polled to completion before, so we should act similar to a
+                // fused iterator and return that it's completed
+                if self.generator.finished {
+                    return Poll::Ready(GeneratorStatus::Completed);
+                }
+                let pinned = self.generator.future.as_mut();
+                // We poll the future we're storing
+                match pinned.poll(cx) {
+                    // Future has returned, generator has completed.
+                    Poll::Ready(v) => {
+                        self.generator.finished = true;
+                        Poll::Ready(GeneratorStatus::Returned(v))
+                    }
+                    // Yield or a random await
+                    Poll::Pending => {
+                        // We check here, because the future might yield for some reason other than
+                        // stream.yield_value()
+                        if let Some(v) = self.generator.receiver.try_recv() {
+                            return Poll::Ready(GeneratorStatus::Yielded(v));
+                        }
+                        Poll::Pending
                     }
                 }
             }
         }
     }
 
-    pub fn resume_async(&mut self) -> impl Future<Output = GeneratorStatus<Y, R>> {
-        MiniGenFuture {
-            generator: &mut *self,
+    pub struct MiniIter<'a, Y>(pub(crate) MiniGen<'a, Y, ()>);
+
+    impl<Y> Iterator for MiniIter<'_, Y> {
+        type Item = Y;
+        fn next(&mut self) -> Option<Self::Item> {
+            match self.0.resume() {
+                GeneratorStatus::Yielded(it) => Some(it),
+                GeneratorStatus::Returned(()) | GeneratorStatus::Completed => None,
+            }
         }
     }
-}
 
-struct MiniGenFuture<'r, 'g, Y, R> {
-    generator: &'r mut MiniGen<'g, Y, R>,
-}
+    impl<Y> Unpin for MiniIter<'_, Y> {}
 
-impl<Y, R> Future for MiniGenFuture<'_, '_, Y, R> {
-    type Output = GeneratorStatus<Y, R>;
+    #[repr(transparent)]
+    struct MiniIterFuture<'r, 'g, Y> {
+        generator: &'r mut MiniIter<'g, Y>,
+    }
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        // The future was polled to completion before, so we should act similar to a
-        // fused iterator and return that it's completed
-        if self.generator.finished {
-            return Poll::Ready(GeneratorStatus::Completed);
-        }
-        let pinned = self.generator.future.as_mut();
-        // We poll the future we're storing
-        match pinned.poll(cx) {
-            // Future has returned, generator has completed.
-            Poll::Ready(v) => {
-                self.generator.finished = true;
-                Poll::Ready(GeneratorStatus::Returned(v))
+    impl<Y: Send> MiniIter<'_, Y> {
+        pub fn resume_async(&mut self) -> impl Future<Output = Option<Y>> {
+            MiniIterFuture {
+                generator: &mut *self,
             }
-            // Yield or a random await
-            Poll::Pending => {
-                // We check here, because the future might yield for some reason other than
-                // stream.yield_value()
-                if let Some(v) = self.generator.receiver.try_recv() {
-                    return Poll::Ready(GeneratorStatus::Yielded(v));
+        }
+    }
+
+    impl<Y> Future for MiniIterFuture<'_, '_, Y> {
+        type Output = Option<Y>;
+
+        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            // The future was polled to completion before, so we should act similar to a
+            // fused iterator and return that it's completed
+            if self.generator.0.finished {
+                return Poll::Ready(None);
+            }
+            let pinned = self.generator.0.future.as_mut();
+            // We poll the future we're storing
+            match pinned.poll(cx) {
+                // Future has returned, generator has completed.
+                Poll::Ready(()) => {
+                    self.generator.0.finished = true;
+                    Poll::Ready(None)
                 }
-                Poll::Pending
+                // Yield or a random await
+                Poll::Pending => {
+                    // We check here, because the future might yield for some reason other than
+                    // stream.yield_value()
+                    if let Some(v) = self.generator.0.receiver.try_recv() {
+                        return Poll::Ready(Some(v));
+                    }
+                    Poll::Pending
+                }
             }
         }
     }
+
+    #[cfg(feature = "stream")]
+    impl<Y: Send> futures_core::Stream for MiniIter<'_, Y> {
+        type Item = Y;
+
+        fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+            let fut = self.resume_async();
+            let fut = std::pin::pin!(fut);
+            fut.poll(cx)
+        }
+    }
+
+    #[derive(Clone, Debug, Copy, PartialEq, Eq)]
+    pub enum GeneratorStatus<Y, R> {
+        Yielded(Y),
+        Returned(R),
+        Completed,
+    }
 }
 
-pub fn generator<'a, Y, R, Fut: Future<Output = R> + 'a, F: FnOnce(Yielder<Y>) -> Fut>(
+pub use generator::*;
+pub use yielder::*;
+
+pub fn generator<'a, Y: Send, R, Fut: Future<Output = R> + 'a, F: FnOnce(Yielder<Y>) -> Fut>(
     func: F,
 ) -> MiniGen<'a, Y, R> {
-    let arc = Arc::new(Mutex::new(None));
-    let (sender, receiver) = (Sender(arc.clone()), Receiver(arc.clone()));
-    let stream = Yielder::<Y> { sender };
+    let inner = Rc::new(Cell::new(None));
+    let (sender, receiver) = (Sender(inner.clone()), Receiver(inner));
+    let stream = Yielder { sender };
 
     let future = func(stream);
     MiniGen {
@@ -236,8 +263,7 @@ pub fn generator<'a, Y, R, Fut: Future<Output = R> + 'a, F: FnOnce(Yielder<Y>) -
         finished: false,
     }
 }
-
-pub fn iterator<'a, Y, Fut: Future<Output = ()> + 'a, F: FnOnce(Yielder<Y>) -> Fut>(
+pub fn iterator<'a, Y: Send, Fut: Future<Output = ()> + 'a, F: FnOnce(Yielder<Y>) -> Fut>(
     func: F,
 ) -> MiniIter<'a, Y> {
     MiniIter(generator(func))
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,65 @@
 use minigen::*;
-fn main() {
-    let count = 10_000_000i32;
+#[tokio::main]
+async fn main() {
+    tokio::task::spawn(func());
+    std::thread::sleep(std::time::Duration::from_secs(10000));
+}
 
-    let generator = iterator(|mut yielder| async move {
-        for i in 0..count {
+async fn func() {
+    let mut generator = generator(|mut yielder| async move {
+        for i in 0..100 {
             yielder.yield_value(i).await;
         }
     });
-    for i in generator {
-        std::hint::black_box(i);
-    }
-
-    let mut v1 = Vec::with_capacity(count as usize);
-    let mut v2 = Vec::with_capacity(count as usize);
-    println!("Starting...");
-    let now = std::time::Instant::now();
-    let mut generator = iterator(|mut yielder| async move {
-        for i in 0..count {
-            yielder.yield_value(i).await;
+    loop {
+        match generator.resume_async().await {
+            GeneratorStatus::Yielded(v) => println!("Yielded: {v:?}"),
+            GeneratorStatus::Returned(v2) => println!("Returned: {v2:?}"),
+            GeneratorStatus::Completed => {
+                println!("completed");
+                break;
+            }
         }
-    });
-    pollster::block_on(async {
-        while let Some(v) = generator.resume().await {
-            v1.push(v);
-        }
-    });
-    println!("Elapsed: {:?}", now.elapsed());
-
-    let now = std::time::Instant::now();
-    for i in std::hint::black_box(It(count, 0)) {
-        v2.push(i);
-    }
-    std::hint::black_box(v1);
-    std::hint::black_box(v2);
-    println!("Elapsed: {:?}", now.elapsed());
-}
-
-struct It(i32, i32);
-impl Iterator for It {
-    type Item = i32;
-    #[inline(never)]
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.1 > self.0 {
-            self.1 += 1;
-            return Some(self.1 - 1);
-        }
-        None
     }
 }
+// use minigen::*;
+// fn main() {
+//     let count = 100_000_000i32;
+
+//     let mut v1 = Vec::with_capacity(count as usize);
+//     let mut v2 = Vec::with_capacity(count as usize);
+//     println!("Starting...");
+
+//     let now = std::time::Instant::now();
+//     for i in It(std::hint::black_box(count), std::hint::black_box(0)) {
+//         v2.push(std::hint::black_box(i));
+//     }
+//     println!("Elapsed: {:?}", now.elapsed());
+
+//     let now = std::time::Instant::now();
+//     let generator = iterator(|mut yielder| async move {
+//         for i in 0..std::hint::black_box(count) {
+//             yielder.yield_value(std::hint::black_box(i)).await;
+//         }
+//     });
+//     for v in generator {
+//         v1.push(std::hint::black_box(v));
+//     }
+//     println!("Elapsed: {:?}", now.elapsed());
+//     assert_eq!(v1.len(), v2.len());
+//     std::hint::black_box(v1);
+//     std::hint::black_box(v2);
+// }
+
+// struct It(i32, i32);
+// impl Iterator for It {
+//     type Item = i32;
+//     #[inline(never)]
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if self.1 < self.0 {
+//             self.1 += 1;
+//             return Some(self.1 - 1);
+//         }
+//         None
+//     }
+// }
